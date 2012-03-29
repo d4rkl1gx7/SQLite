d651f0104e34c11df9e2bb5e613cbe21fd1d1308
ipodishima <marian.paul2@gmail.com>
2012-03-29 14:38:43 +0200

    Initial Commit

---
+++ SQLite/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SQLite
+//
+//  Created by Marian PAUL on 29/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
